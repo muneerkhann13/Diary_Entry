3ce5fced4d70bb6999d12d2d6b826de92d5efa98
Muneerr Arif <muneerkhann13@gmail.com>
2015-06-11 16:44:23 +0530

    Create js.php

---
+++ configure/js.php
@@ -0,0 +1,12 @@
+<?php
+//js files:
+
+?>
+	<!--jQuery-->
+	<script src="//code.jquery.com/jquery-1.10.2.min.js"></script> 
+		
+	<!--jQuery-UI-->
+	<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></scrip>
+		
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
