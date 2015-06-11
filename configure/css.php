91a79b8952bd7a3978782341bce69621158a8891
Muneerr Arif <muneerkhann13@gmail.com>
2015-06-11 16:43:58 +0530

    Create css.php

---
+++ configure/css.php
@@ -0,0 +1,16 @@
+<?php
+//css files:
+
+?>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+		
+<!--jQuery-css-->
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+
+<!--font-awesome-->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+		
